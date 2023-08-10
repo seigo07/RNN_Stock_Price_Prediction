50258fa64aef9c8a483f146896b9c2fa788f99fa
seigo07 <seven_8s@icloud.com>
2023-08-10 21:21:54 +0100

    add TestCleanData

--- src/functions_test.py
+++ src/functions_test.py
@@ -1,9 +1,10 @@
 import unittest
 from unittest.mock import patch
 import pandas as pd
+import numpy as np
 
 # Assuming your function is in a module named 'data_loader'
-from src.functions import load_data
+from src.functions import load_data, clean_data
 
 INVALID_TICKER_ERROR = "Invalid ticker. Please enter a valid ticker"  # Assuming you have a constant for this
 
@@ -31,6 +32,33 @@ class TestLoadData(unittest.TestCase):
             load_data("INVALID")
 
         self.assertEqual(cm.exception.code, INVALID_TICKER_ERROR)
+
+
+class TestCleanData(unittest.TestCase):
+
+    def setUp(self):
+        self.dataset = pd.DataFrame({
+            'Open': [100, 101, 102, 103, 104],
+            'Close': [101, 102, np.nan, 105, 110],
+            'Volume': [1000, 1100, 1200, 1300, 1400]
+        }, index=pd.to_datetime(['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-03', '2021-01-05']))
+
+    def test_remove_nan_values(self):
+        cleaned_data, _ = clean_data(self.dataset)
+        self.assertFalse(cleaned_data['Close'].isnull().any())
+
+    def test_remove_duplicate_dates(self):
+        cleaned_data, _ = clean_data(self.dataset)
+        self.assertEqual(cleaned_data.index.duplicated().sum(), 0)
+
+    def test_clip_close_values(self):
+        cleaned_data, _ = clean_data(self.dataset)
+        self.assertTrue((cleaned_data['Close'] <= np.percentile(self.dataset['Close'].dropna(), 99)).all())
+        self.assertTrue((cleaned_data['Close'] >= 0).all())
+
+    def test_reshape_close_values(self):
+        _, data = clean_data(self.dataset)
+        self.assertEqual(data.shape, (4, 1))
 
 
 if __name__ == '__main__':
